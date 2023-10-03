6c40ac96580fe92c9b494105d920cb22a179b19f
crissegura <103970933+crissegura@users.noreply.github.com>
2023-10-03 20:41:11 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -10,5 +10,6 @@ var estiloPopup =
       'maxWidth': '500'
       }
 var marker = L.marker([-34.8981541,-58.438131]).addTo(map);
-marker.bindPopup("<h1>Paraje La Lata, Buenos Aires</h1><img src='img/talaia.jpeg' alt='La Talaia'><p>Aca me caso ahre</p>.",estiloPopup).openPopup();
+marker.bindPopup("<h1>Paraje La Lata, Buenos Aires</h1><img src='' alt='La Talaia'><p>Aca me caso ahre</p><a href='https://www.google.com/maps/place/Comunidad+Cristiana+Zona+Sur/@-34.8981541,-58.438131,15z/data=!4m6!3m5!1s0x95bcd5936fa98483:0x46fcc942719ba15e!8m2!3d-34.8981541!4d-58.438131!16s%2Fg%2F11b8z01d_c?entry=ttu' target='-'>Abrir en Google Maps</a>",estiloPopup).openPopup();
 var popup = L.popup();
+
