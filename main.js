281f879a6a1254c7c5013541b166f27799819a11
crissegura <103970933+crissegura@users.noreply.github.com>
2023-10-03 21:27:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 let map = L.map('map').setView([-34.8981541,-58.438131],12)
 
-//Agregar tilelAyer mapa base desde openstreetmap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var estiloPopup =
+let estiloPopup =
       {
       'maxWidth': '500'
       }
-var marker = L.marker([-34.8981541,-58.438131]).addTo(map);
-marker.bindPopup("<h1>Paraje La Lata, Buenos Aires</h1><img src='' alt='La Talaia'><p>Aca me caso ahre</p><a href='https://www.google.com/maps/place/Comunidad+Cristiana+Zona+Sur/@-34.8981541,-58.438131,15z/data=!4m6!3m5!1s0x95bcd5936fa98483:0x46fcc942719ba15e!8m2!3d-34.8981541!4d-58.438131!16s%2Fg%2F11b8z01d_c?entry=ttu' target='-'>Abrir en Google Maps</a>",estiloPopup).openPopup();
-var popup = L.popup();
+let marker = L.marker([-34.8981541,-58.438131]).addTo(map);
+
+marker.bindPopup("<h1>Paraje La Lata, Buenos Aires</h1><img src='' alt='Paraje La Lata'><p>Aca me caso ahre</p><a href='https://www.google.com/maps/place/Comunidad+Cristiana+Zona+Sur/@-34.8981541,-58.438131,15z/data=!4m6!3m5!1s0x95bcd5936fa98483:0x46fcc942719ba15e!8m2!3d-34.8981541!4d-58.438131!16s%2Fg%2F11b8z01d_c?entry=ttu' target='-'>Abrir en Google Maps</a>",estiloPopup).openPopup();
+let popup = L.popup();
 
